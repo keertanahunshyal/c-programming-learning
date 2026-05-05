006adc6fc754fc02ad3df24b0a67b1f5d6f9e97a
keertanahunshyal <keertanahunshyal2006@gmail.com>
2026-05-05 23:09:04 +0530

    Create day5_switch_case.c

---
+++ day5_switch_case.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    char operator;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    switch(operator) {
+        case '+':
+            printf("Result: %d\n", num1 + num2);
+            break;
+
+        case '-':
+            printf("Result: %d\n", num1 - num2);
+            break;
+
+        case '*':
+            printf("Result: %d\n", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0)
+                printf("Result: %d\n", num1 / num2);
+            else
+                printf("Error: Division by zero is not allowed.\n");
+            break;
+
+        default:
+            printf("Invalid operator.\n");
+    }
+
+    return 0;
+}v
